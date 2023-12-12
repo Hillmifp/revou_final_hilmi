7c9821f5477b6a376a24dcda7be24f791d13b2b3
Hillmifp <hilmifauzi06@gmail.com>
2023-12-12 19:57:36 +0700

    fix: disasterRoutes in app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-// src/server.js
 const express = require("express");
-const routes = require("./src/routes/disastersRoutes"); // Corrected path
+const disastersRoutes = require("./src/routes/disastersRoutes"); // Corrected path
 
 const app = express();
 const port = 3000;
@@ -8,7 +7,7 @@ const port = 3000;
 app.use(express.json());
 
 // Mount the aggregated router
-app.use("/disasters", routes);
+app.use("/disasters", disastersRoutes); // Corrected variable name
 
 app.get("/status", (req, res) => {
   res.status(200).send({ message: "Hilmi Sukses" });
