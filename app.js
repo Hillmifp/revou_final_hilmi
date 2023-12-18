b11078dac9697c77fd1f997aefb3ed92187a98e3
Hillmifp <hilmifauzi06@gmail.com>
2023-12-18 19:18:24 +0700

    feat: update CORS

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const disastersRoutes = require("./src/routes/disastersRoutes"); // Corrected path
 
 const app = express();
 const port = 3000;
+
+app.use(cors());
 
 app.use(express.json());
 
